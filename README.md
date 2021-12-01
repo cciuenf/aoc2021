ee060627c34b64ba424d1aed2e4c054693ca4b94
matdsoupe <matheus_pessanha2001@outlook.com>
2021-12-01 10:35:09 -0300

    initial commit :tada:

---
+++ README.md
@@ -0,0 +1,15 @@
+---
+title: Advent of Code 2021 UENF
+
+---
+
+<img align="left" width="250" height="200" src="./assets/uenf.png" />
+<img align="right" width="250" height="200" src="./assets/cc.png" />
+
+Este repositório abriga as soluções dos desafio do [Advent of Code 2021](https://adventofcode.com) implementadas por alunes do curso de Ciência da Computação da [UENF](https://uenf.br/portal/).
+
+## Participantes e soluções
+- [João Pinto ()](./joao_pinto)
+- [João Vitor()](./joao_vitor)
+- [Math (Haskell)](./math/)
+- [Otávio ()](./otavio)
---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Soluções do Advent of Code 2021";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+
+        elixir_1_12 = pkgs.beam.packages.erlangR24.elixir.override {
+          version = "1.12.2";
+          minimumOTPVersion = "24";
+          sha256 = "1f8b63x2klhdz1fq1dgbrqs7x6rq309abzq48gicmd0vprfhc641";
+        };
+      in {
+        devShell = pkgs.mkShell {
+          name = "aoc2021";
+          buildInputs = with pkgs; [
+            # base pkgs
+            gnumake
+            readline
+            openssl
+            zlib
+            libxml2
+            curl
+            libiconv
+            glibcLocales
+
+            # langs
+            ## C
+            gcc
+            ## elixir
+            elixir_1_12
+            ## haskell
+            stack ghc haskellPackages.fourmolu
+          ] ++ pkgs.lib.optional stdenv.isLinux [
+            inotify-tools
+            gtk-engine-murrine
+          ] ++ pkgs.lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+            CoreFoundation
+            CoreServices
+          ]);
+        };
+      });
+}
