de60fa122822a1dbab335caa381d729c873c886f
matdsoupe <matheus_pessanha2001@outlook.com>
2021-12-02 22:32:13 -0300

    feat: added haskell-stack project

---
+++ math/haskell/app/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import qualified Day01 as Day01
+
+main :: IO ()
+main = do
+  Day01.main Day01.dataFile
--- math/haskell/src/Day01.hs
+++ math/haskell/src/Day01.hs
@@ -1,4 +1,4 @@
-module Day1 (main) where
+module Day01 (main, dataFile) where
 
 depthIncreasement :: [Int] -> Int
 depthIncreasement = length . filterNegatives . differences
@@ -12,9 +12,9 @@ toSlidingWindow xs = (sum . triplet) xs : toSlidingWindow (tail xs)
   where
     triplet = take 3
 
-main :: IO ()
-main = do
-    input <- map read . lines <$> readFile dataFile
+main :: FilePath -> IO ()
+main inputPath = do
+    input <- map read . lines <$> readFile inputPath
     let largerDepths = depthIncreasement input
     putStrLn $ (show largerDepths) ++ " measurements are larger than the previous measurement"
     let largerSums = (depthIncreasement . toSlidingWindow) input
