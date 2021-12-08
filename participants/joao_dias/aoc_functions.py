781faaaf26b0bfeb0b4f1abbfe291c75a4de3991
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2021-12-07 23:05:56 -0300

    Did some refactoring to the abstracted functions file
    removed some  useless functions
    changed " to '
    defined some strings as default input for functions
    upgraded the comments of the functions

--- participants/joao_dias/aoc_functions.py
+++ participants/joao_dias/aoc_functions.py
@@ -4,69 +4,70 @@
 import importlib
 import pandas as pd
 
-def get_daynum(day_number):
-    '''Returns the string "daynumber_"'''
+def get_daynum(day_number, day_prefix='day', terminator='_'):
+    ''' receives:   a numeric value that refers to the day
+                    the prefix of the day
+                    a terminator character
+        returns:    a string "day"+number+"terminator" '''
     day_str = ''
     if day_number < 10:
         day_str += '0'
     day_str += str(day_number)
-    day = 'day'+day_str+'_'
+    day = day_prefix+day_str+terminator
     return day
 
-def get_folder_name(day, folder_name='files'):
-    '''returns the folder name'''
-    return day + folder_name
+def get_path(day_number, middle_char, file_sufix, folder_sufix='files'):
+    ''' receives:   a numeric value that refers to the day
+                    the middle char that separates folder and datafile names
+                    the sufix of the file containing informations
+                    the sufix of the folder that contains the info of a day-challenge
+        returns:    the path to read data'''
+    day = get_daynum(day_number)
+    return day+folder_sufix+middle_char+day+file_sufix
 
-def get_datafile_name(day, file_name='data.txt'):
-    '''returns the name of the data file'''
-    return day+file_name
-
-def get_path(day, middle_char = '/'):
-    '''returns the path to read data'''
-    return get_folder_name(day)+middle_char+get_datafile_name(day)
-
-def get_module_to_import(day):
-    '''get module name to be imported'''
-    folder_name = get_folder_name(day)
-    file_name = day+'solution'
-    module_to_import = folder_name+'.'+file_name
-    return module_to_import
-
-def get_imported_package(day):
-    '''returns the object(?) of a file that has the functions part1 and part2'''
-    module_to_import = get_module_to_import(day)
+def get_imported_package(day_number, middle_char = '.', file_sufix = 'solution'):
+    ''' receives:   a numeric value that refers to the day
+                    the middle char that separates folder and datafile names
+                    the sufix of the file containing the functions
+        returns:    the object(?) of a file that has the functions part1 and part2'''
+    module_to_import = get_path(day_number, middle_char, file_sufix)
     imported_module = importlib.import_module(module_to_import)
     return imported_module
 
-def clean_data(data):
-    """function to clean the dataframe from pandas"""
-    data_list = data.values.tolist()
+def dataframe_to_list(dataframe_from_pandas):
+    ''' receives:   a dataframe from pandas
+        returns:    the data in a list'''
+    dataframe_list = dataframe_from_pandas.values.tolist()
     cleaned_list = []
-    for elem in data_list:
+    for elem in dataframe_list:
         cleaned_list.append(elem[0])
     return cleaned_list
 
-def get_data(day_number):
-    """Function to return the vector of the data"""
-    path = get_path(day_number)
-    received_data = pd.read_csv(path, header=None)
-    cleaned_data = clean_data(received_data)
-    return cleaned_data
+def get_data(day_number=0, middle_char = '/', file_sufix = 'data.txt'):
+    ''' receives:   a numeric value that refers to the day
+                    the middle char that separates folder and datafile names
+                    the sufix of the file containing data
+        returns:    a list of data'''
+    path = get_path(day_number, middle_char, file_sufix)
+    dataframe_from_pandas = pd.read_csv(path, header=None)
+    data_list = dataframe_to_list(dataframe_from_pandas)
+    return data_list
 
 def challenge_of_the_day(day_number):
-    '''Executes the functions from a file of the day received'''
-    day = get_daynum(day_number)
-    data = get_data(day)
-    daily_func = get_imported_package(day)
+    ''' receives:   a numeric value that refers to the day challenge
+        and then executes the functions from this day's challenge'''
+    data = get_data(day_number)
+    daily_func = get_imported_package(day_number)
     daily_func.part1(data)
     daily_func.part2(data)
 
-def spaces(num=3):
-    '''Just spacing things n times'''
-    print(num*"\n")
+def spaces(number_of_spaces=3):
+    ''' receives:   the number of spaces to have
+        then:       prints "\\n" n times'''
+    print(number_of_spaces*'\n')
 
-def main (day_number=1):
-    '''Main function calling the challenge day function spaced out'''
+def main(day_number=2):
+    '''spaces the print on terminal and calls the challenge of the day'''
     spaces()
     challenge_of_the_day(day_number)
     spaces()
